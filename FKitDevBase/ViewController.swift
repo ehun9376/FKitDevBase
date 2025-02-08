a609fc8616664f6f166ed4e6a6fbd81efadd2c4f
陳逸煌 <joh1234567892001@gmail.com>
2025-02-08 11:16:46 +0800

    Initial Commit

---
+++ FKitDevBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FKitDevBase
+//
+//  Created by 陳逸煌 on 2025/2/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FKitDevBaseTests/FKitDevBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  FKitDevBaseTests.swift
+//  FKitDevBaseTests
+//
+//  Created by 陳逸煌 on 2025/2/8.
+//
+
+import Testing
+@testable import FKitDevBase
+
+struct FKitDevBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
